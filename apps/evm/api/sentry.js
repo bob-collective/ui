3c2094bfd5ab49cc88aa28ddd9cf56e3e10c84df
ns212 <73105077+ns212@users.noreply.github.com>
2024-06-12 18:58:21 +0100

    chore: sentry - add tunnel (#4)

---
+++ apps/evm/api/sentry.js
@@ -0,0 +1,47 @@
+const SENTRY_HOST = 'o4504853415329792.ingest.us.sentry.io';
+const SENTRY_PROJECT_IDS = ['4507407322316800'];
+
+export const config = {
+  runtime: 'edge'
+};
+
+async function extractBody(request) {  
+  const dec = new TextDecoder();
+  const reader = request.body.getReader();
+  let body = ""
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) return body;
+
+    body = body + dec.decode(value)
+  }
+}
+
+export default async (request) => {
+  try {
+    const envelope = await extractBody(request);
+    const piece = envelope.split('\n')[0];
+    const header = JSON.parse(piece);
+    const dsn = new URL(header['dsn']);
+    const project_id = dsn.pathname?.replace('/', '');
+
+    if (dsn.hostname !== SENTRY_HOST) {
+      throw new Error(`Invalid sentry hostname: ${dsn.hostname}`);
+    }
+
+    if (!project_id || !SENTRY_PROJECT_IDS.includes(project_id)) {
+      throw new Error(`Invalid sentry project id: ${project_id}`);
+    }
+
+    const upstream_sentry_url = `https://${SENTRY_HOST}/api/${project_id}/envelope/`;
+
+    await fetch(upstream_sentry_url, { method: 'POST', body: envelope });
+
+    return new Response(null, { status: 200 });
+  } catch (e) {
+    console.error('error tunneling to sentry', e); // eslint-disable-line no-console
+
+    return new Response('error tunneling to sentry', { status: 500 });
+  }
+};
