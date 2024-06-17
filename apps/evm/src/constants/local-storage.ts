be290e53fc02eebd0c568b4ee678c33ccd2a26b1
Daniel Simão <rui.daniel.simao@gmail.com>
2024-06-17 11:46:23 +0100

    feat(evm): add banner carousel (#8)

--- apps/evm/src/constants/local-storage.ts
+++ apps/evm/src/constants/local-storage.ts
@@ -1,5 +1,4 @@
 enum LocalStorageKey {
-  HIDE_ECOSYSTEM_BANNER = 'hideEcosystemBanner',
   HIDE_INFO_TAB_UNVISITED_MARK = 'hideInfoTabUnvisitedMark'
 }
 export { LocalStorageKey };
--- knip.config.js
+++ knip.config.js
@@ -2,7 +2,12 @@
 const config = {
   ignoreWorkspaces: ['apps/e2e'],
   ignoreBinaries: ['synpress:run'],
-  ignore: ['apps/evm/src/pages/Home/**', 'apps/evm/api/**', 'packages/currency/src/constants.ts']
+  ignore: [
+    'apps/evm/src/pages/Home/**',
+    'apps/evm/api/**',
+    'packages/currency/src/constants.ts',
+    'apps/evm/vite.config.ts'
+  ]
 };
 
 export default config;
