d6b1cf48e4f6d876bc37dec5d8b4d40689084e1c
Tom Jeatt <hello@tomjeatt.com>
2024-06-18 16:14:23 +0100

    chore: update banner text

--- apps/evm/src/pages/Bridge/components/BannerCarousel/OnrampBanner.tsx
+++ apps/evm/src/pages/Bridge/components/BannerCarousel/OnrampBanner.tsx
@@ -11,7 +11,7 @@ const OnrampBanner = ({ onPress }: OnrampBannerProps) => (
     <StyledBannerContent direction='column'>
       <Flex alignItems='center'>
         <H1 size='2xl' weight='bold'>
-          BOB Onramp is live!
+          BOB Gateway is live!
         </H1>
       </Flex>
       <P color='grey-200'>The fastest and easiest way to bridge BTC to BOB</P>
