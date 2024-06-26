83320c3cf3165338d6e9e7aea03628189c8a28a9
Tom Jeatt <hello@tomjeatt.com>
2024-06-26 12:52:24 +0100

    feature: simple number spinner

--- apps/evm/src/pages/Fusion/components/UserStats/Spice.tsx
+++ apps/evm/src/pages/Fusion/components/UserStats/Spice.tsx
@@ -1,6 +1,8 @@
 import { useLocale } from '@gobob/ui';
 import { DlGroup, Dt, Dd } from '@gobob/ui';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useRef } from 'react';
+import { CountUp } from 'countup.js';
 
 import { useGetUser } from '../../../../hooks';
 
@@ -8,14 +10,24 @@ const Spice = () => {
   const { locale } = useLocale();
   const { t } = useTranslation();
 
+  let spiceCountRef = useRef(null);
+
   const { data: user } = useGetUser();
+
+  useEffect(() => {
+    const spiceAnimation = new CountUp(spiceCountRef.current as any, user?.leaderboardRank?.total_reward_points || 0);
+
+    spiceAnimation.start();
+  }, [user]);
 
   return (
     <DlGroup alignItems='flex-start' direction='column'>
       <Dt size='s' weight='semibold'>
         {t('fusion.userStats.spice')}
       </Dt>
-      <Dd weight='bold'>{Intl.NumberFormat(locale).format(user?.leaderboardRank?.total_reward_points ?? 0)}</Dd>
+      <Dd ref={spiceCountRef} weight='bold'>
+        {Intl.NumberFormat(locale).format(user?.leaderboardRank?.total_reward_points ?? 0)}
+      </Dd>
     </DlGroup>
   );
 };
