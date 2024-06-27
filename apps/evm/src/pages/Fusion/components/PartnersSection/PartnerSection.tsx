6361a743446159bbe0c5d3f77971d723203aa156
Tom Jeatt <hello@tomjeatt.com>
2024-06-27 16:29:26 +0100

    refactor: lowercase string before checking

--- apps/evm/src/pages/Fusion/components/PartnersSection/PartnerSection.tsx
+++ apps/evm/src/pages/Fusion/components/PartnersSection/PartnerSection.tsx
@@ -28,7 +28,7 @@ const PartnersSection = () => {
       return (
         partnerData.partners
           // NOTE: Remove category check when adding quest project cards
-          .filter((result) => result.show_on_app_store && result.category !== 'Quest')
+          .filter((result) => result.show_on_app_store && result.category.toLowerCase() !== 'quest')
           .sort((a, b) => Number(b.points_distributed_per_hour) - Number(a.points_distributed_per_hour))
       );
     },
