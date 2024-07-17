b739e410fc05a0f26505f8f1ea922ea31475d978
Tom Jeatt <hello@tomjeatt.com>
2024-07-17 13:20:05 +0100

    chore: revert erroneous code change

--- apps/evm/vite.config.ts
+++ apps/evm/vite.config.ts
@@ -6,23 +6,18 @@ import wasm from 'vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd());
 
   return {
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV)
-    },
     plugins: [
       react(),
       nodePolyfills(),
       wasm(),
       sentryVitePlugin({
-        applicationKey: 'bob-ui-application-key',
-        authToken: env.VITE_SENTRY_AUTH_TOKEN,
         org: 'distributed-crafts',
-        project: 'bob-ui'
+        project: 'bob-ui',
+        applicationKey: 'bob-ui-application-key',
+        authToken: process.env.SENTRY_AUTH_TOKEN
       })
     ],
     build: {
