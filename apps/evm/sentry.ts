0585fc25323a6df0816d2fb204c31348912d631c
Tom Jeatt <hello@tomjeatt.com>
2024-07-23 12:46:53 +0100

    chore: tag third party errors

--- apps/evm/sentry.ts
+++ apps/evm/sentry.ts
@@ -24,19 +24,19 @@ Sentry.init({
       createRoutesFromChildren,
       matchRoutes
     }),
-    Sentry.replayIntegration()
-    // Sentry.thirdPartyErrorFilterIntegration({
-    //   // Specify the application keys that you specified in the Sentry bundler plugin
-    //   filterKeys: ['bob-ui-application-key'],
+    Sentry.replayIntegration(),
+    Sentry.thirdPartyErrorFilterIntegration({
+      // Specify the application keys that you specified in the Sentry bundler plugin
+      filterKeys: ['bob-ui-application-key'],
 
-    //   // Defines how to handle errors that contain third party stack frames.
-    //   // Possible values are:
-    //   // - 'drop-error-if-contains-third-party-frames'
-    //   // - 'drop-error-if-exclusively-contains-third-party-frames'
-    //   // - 'apply-tag-if-contains-third-party-frames'
-    //   // - 'apply-tag-if-exclusively-contains-third-party-frames'
-    //   behaviour: 'drop-error-if-contains-third-party-frames'
-    // })
+      // Defines how to handle errors that contain third party stack frames.
+      // Possible values are:
+      // - 'drop-error-if-contains-third-party-frames'
+      // - 'drop-error-if-exclusively-contains-third-party-frames'
+      // - 'apply-tag-if-contains-third-party-frames'
+      // - 'apply-tag-if-exclusively-contains-third-party-frames'
+      behaviour: 'apply-tag-if-contains-third-party-frames'
+    })
   ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
