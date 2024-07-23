b870a84f600e957d1ff0638b6e3f177de23b9f2d
Rui Simão <rui.daniel.simao@gmail.com>
2024-07-23 16:37:14 +0100

    feat: improve dynamic state handling

--- apps/hybrid-pay/src/hooks/index.ts
+++ apps/hybrid-pay/src/hooks/index.ts
@@ -3,3 +3,5 @@ export * from './useTokens';
 export * from './useKernelClient';
 export * from './useTotalBalance';
 export * from './useGetTransactions';
+export * from './useIsDynamicSmartAccount';
+export * from './useDynamicAddress';
