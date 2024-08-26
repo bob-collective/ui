e1173efba187d3c4eab8fde1fa18f382682feee0
Tom Jeatt <hello@tomjeatt.com>
2024-08-26 09:04:35 +0100

    chore: update min BTC transfer amount

--- apps/evm/src/pages/Bridge/components/BridgeForm/BtcBridgeForm.tsx
+++ apps/evm/src/pages/Bridge/components/BridgeForm/BtcBridgeForm.tsx
@@ -41,7 +41,7 @@ type BtcBridgeFormProps = {
   onFailOnRamp: () => void;
 };
 
-const MIN_DEPOSIT_AMOUNT = 0;
+const MIN_DEPOSIT_AMOUNT = 1000;
 
 const gasEstimatePlaceholder = CurrencyAmount.fromRawAmount(BITCOIN, 0n);
 
