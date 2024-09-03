3bdbb05f55a934f2c07a7c13e3294e9b73b96bce
Daniel Simão <rui.daniel.simao@gmail.com>
2024-09-03 11:02:17 +0100

    fix(sats-wagmi): use external lib (#123)

--- packages/sats-wagmi/src/connectors/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './xverse';
-export * from './leather';
-export * from './unisat';
-export * from './base';
-export * from './mm-snap';
--- packages/sats-wagmi/src/index.ts
+++ packages/sats-wagmi/src/index.ts
@@ -1,4 +1,1 @@
-export { SatsWagmiConfig, useSatsWagmi } from './provider';
-export * from './hooks';
-export * from './connectors';
-export { AddressType as BtcAddressType } from 'bitcoin-address-validation';
+export * from '@gobob/sats-wagmi';
