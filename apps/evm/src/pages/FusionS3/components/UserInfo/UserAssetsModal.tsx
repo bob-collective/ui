5a213e52627f32ea68fa51367b60909446d6375a
Rui Simão <rui.daniel.simao@gmail.com>
2024-09-09 10:47:41 +0100

    feat(evm): add multipliers

--- apps/evm/src/pages/FusionS3/components/UserInfo/UserAssetsModal.tsx
+++ apps/evm/src/pages/FusionS3/components/UserInfo/UserAssetsModal.tsx
@@ -2,10 +2,12 @@ import { NATIVE } from '@gobob/tokens';
 import { Avatar, Flex, List, ListItem, Modal, ModalBody, ModalHeader, ModalProps, P, Span, useLocale } from '@gobob/ui';
 import { Address, isAddressEqual } from 'viem';
 import { ERC20Token } from '@gobob/currency';
+import { Spice } from '@gobob/icons';
 
-import { useGetUser, useTokens } from '../../../../hooks';
+import { useGetUser } from '../../../../hooks';
 import { L1_CHAIN, L2_CHAIN } from '../../../../constants';
 import { useGetTokensInfo } from '../../hooks/useGetTokensInfo';
+import { useBridgeTokens } from '../../../Bridge/hooks';
 
 type Props = {};
 
@@ -17,35 +19,41 @@ const nativeToken = NATIVE[L2_CHAIN];
 
 const UserAssetsModal = (props: UserAssetsModalProps): JSX.Element => {
   const { data: user } = useGetUser();
-  const { data: tokens } = useTokens(L1_CHAIN);
+  const { data: tokens } = useBridgeTokens(L1_CHAIN, L2_CHAIN);
   const { locale } = useLocale();
 
-  const { data: tokenInfo } = useGetTokensInfo({ enabled: !!props.isOpen });
+  const { data: tokensInfo } = useGetTokensInfo({ enabled: !!props.isOpen });
 
   const data = tokens
-    ?.map((item) => {
-      if (item.currency.isNative) {
-        const stats = user?.depositStats?.find((token) => token.token_name === 'ethereum');
+    ?.map((token) => {
+      if (token.l1Currency.isNative) {
+        const stats = user?.depositStats?.find((item) => item.token_name === 'ethereum');
+
+        const tokenInfo = tokensInfo?.find((info) => info.symbol === nativeToken.symbol);
 
         return {
           amount: stats?.total_amount || 0,
-          logoUrl: item.raw.logoUrl,
+          logoUrl: token.l1Token.logoUrl,
           symbol: nativeToken.symbol,
-          totalUsd: Number(stats?.total_usd || 0)
+          totalUsd: Number(stats?.total_usd || 0),
+          multiplier: tokenInfo?.multiplier
         };
       }
 
       const stats = user?.depositStats?.find(
-        (token) =>
-          item.currency.isToken &&
-          isAddressEqual((item.currency as ERC20Token)?.address, token?.token_address as Address)
+        (item) =>
+          token.l1Currency.isToken &&
+          isAddressEqual((token.l1Currency as ERC20Token)?.address, item?.token_address as Address)
       );
+
+      const tokenInfo = tokensInfo?.find((info) => isAddressEqual(info.l2_address as Address, token.l2Token.address));
 
       return {
         amount: stats?.total_amount || 0,
-        logoUrl: item.raw.logoUrl,
-        symbol: item.currency.symbol,
-        totalUsd: Number(stats?.total_usd || 0)
+        logoUrl: token.l1Token.logoUrl,
+        symbol: token.l1Token.symbol,
+        totalUsd: Number(stats?.total_usd || 0),
+        multiplier: tokenInfo?.multiplier
       };
     })
     .sort((a, b) => b.totalUsd - a.totalUsd);
@@ -60,14 +68,24 @@ const UserAssetsModal = (props: UserAssetsModalProps): JSX.Element => {
         <List direction='column' gap='md'>
           {data?.map((item, key) => (
             <ListItem key={key} alignItems='center' flex={1} justifyContent='space-between'>
-              <Flex alignItems='center' gap='s'>
+              <Flex alignItems='center' flex='1 1 30%' gap='s'>
                 <Avatar size='2xl' src={item.logoUrl} />
                 <Span>{item.symbol}</Span>
               </Flex>
-              <P>
+              <P color={item.totalUsd > 0 ? 'light' : 'grey-50'} style={{ flex: '1 1 45%' }}>
                 {item.amount} (
                 {Intl.NumberFormat(locale, { style: 'currency', currency: 'USD' }).format(Number(item.totalUsd))})
               </P>
+              {item.multiplier ? (
+                <Flex alignItems='center' flex='1 1 25%' gap='md' justifyContent='flex-end'>
+                  <P color='grey-50' size='s'>
+                    {item.multiplier}x
+                  </P>
+                  <Spice size='s' />
+                </Flex>
+              ) : (
+                <Flex flex='1 1 25%' />
+              )}
             </ListItem>
           ))}
         </List>
