14e787930de70ac66c24c802be29b28f1e24ce6a
Rui Simão <rui.daniel.simao@gmail.com>
2024-09-11 18:19:28 +0100

    feat(evm): add barometer

--- apps/evm/src/pages/FusionS3/components/SeasonInfo/Barometer.tsx
+++ apps/evm/src/pages/FusionS3/components/SeasonInfo/Barometer.tsx
@@ -1,25 +1,108 @@
-import { CSSProperties } from 'styled-components';
-import { Flex } from '@gobob/ui';
+import { CSSProperties, useTheme } from 'styled-components';
+import { useLocale, useMediaQuery } from '@gobob/ui';
 
-import { StyledFill, StyledFilledTrack, StyledTrack, StyledTrackSubtitle, StyledTrackTitle } from './Barometer.style';
+import {
+  StyledBarometer,
+  StyledFill,
+  StyledFilledTrackWrapper,
+  StyledFirstTrack,
+  StyledSecondTrack,
+  StyledStep,
+  StyledStepBar,
+  StyledTrack,
+  StyledTrackSubtitle,
+  StyledTrackTitle
+} from './Barometer.style';
+
+const getPercentage = (value: number, minValue: number, maxValue: number) => (value - minValue) / (maxValue - minValue);
+
+const Step = ({
+  currentValue,
+  maxValue,
+  minValue,
+  value,
+  showBar: showBarProp,
+  showValue: showValueProp
+}: {
+  currentValue: number;
+  minValue: number;
+  maxValue: number;
+  value: number;
+  showBar?: boolean;
+  showValue?: boolean;
+}) => {
+  const { locale } = useLocale();
+  const theme = useTheme();
+  const isTable = useMediaQuery(theme.breakpoints.down('md'));
+
+  const percentage = getPercentage(value, minValue, maxValue) * 100;
+
+  const showBar =
+    showBarProp !== undefined ? showBarProp : isTable ? currentValue < value && value - currentValue < 100 : true;
+  const showValue =
+    showValueProp !== undefined ? showValueProp : isTable ? currentValue < value && value - currentValue < 100 : true;
+
+  return (
+    <>
+      {showBar && <StyledStepBar style={{ left: `${percentage}%`, transform: 'translateX(-50%)' }} />}
+      {showValue && (
+        <StyledStep
+          key={value}
+          color='grey-50'
+          size='xs'
+          style={value < maxValue ? { left: `${percentage}%`, transform: 'translateX(-50%)' } : { right: 0 }}
+        >
+          {Intl.NumberFormat(locale, { notation: 'compact', currency: 'USD' }).format(value * 1000000)}
+        </StyledStep>
+      )}
+    </>
+  );
+};
 
 const Barometer = () => {
-  // const percentage = (value - minValue) / (maxValue - minValue);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('s'));
+  const isTable = useMediaQuery(theme.breakpoints.down('md'));
 
-  const percentage = 0.5;
+  const value = 39;
+
+  const minValue = 35;
+  const maxValue = 300;
+
+  const percentage = getPercentage(value, minValue, maxValue);
+
   const barStyle: CSSProperties = { width: `${Math.round(percentage * 100)}%` };
 
   return (
-    <Flex>
-      <StyledFilledTrack>
-        <StyledTrackTitle>S1</StyledTrackTitle>
-        <StyledTrackSubtitle>Pre Launch</StyledTrackSubtitle>
-      </StyledFilledTrack>
-      <StyledFilledTrack />
-      <StyledTrack>
-        <StyledFill style={barStyle} />
-      </StyledTrack>
-    </Flex>
+    <StyledBarometer>
+      <StyledFilledTrackWrapper>
+        <StyledTrackTitle size='xs'>S1</StyledTrackTitle>
+        <StyledTrackSubtitle noWrap color='grey-50' size='xs'>
+          Pre Launch
+        </StyledTrackSubtitle>
+        <StyledFirstTrack />
+      </StyledFilledTrackWrapper>
+      <StyledFilledTrackWrapper>
+        <StyledTrackTitle size='xs'>S2</StyledTrackTitle>
+        <StyledTrackSubtitle color='grey-50' size='xs'>
+          Bootstrapping
+        </StyledTrackSubtitle>
+        <StyledSecondTrack />
+      </StyledFilledTrackWrapper>
+      <StyledFilledTrackWrapper>
+        <StyledTrackTitle size='xs'>Season 3</StyledTrackTitle>
+        <Step showValue currentValue={value} maxValue={maxValue} minValue={minValue} value={35} />
+        {!isTable && <Step currentValue={value} maxValue={maxValue} minValue={minValue} value={50} />}
+        <Step currentValue={value} maxValue={maxValue} minValue={minValue} value={100} />
+        <Step currentValue={value} maxValue={maxValue} minValue={minValue} value={150} />
+        <Step currentValue={value} maxValue={maxValue} minValue={minValue} value={200} />
+        {!isMobile && <Step currentValue={value} maxValue={maxValue} minValue={minValue} value={250} />}
+        <Step showValue currentValue={value} maxValue={maxValue} minValue={minValue} showBar={false} value={300} />
+        <StyledTrack>
+          <StyledFill style={barStyle} />
+        </StyledTrack>
+      </StyledFilledTrackWrapper>
+    </StyledBarometer>
   );
 };
 
--- apps/evm/src/pages/FusionS3/components/SeasonInfo/SeasonInfo.tsx
+++ apps/evm/src/pages/FusionS3/components/SeasonInfo/SeasonInfo.tsx
@@ -1,5 +1,4 @@
 import { Chip, Flex, H1, P } from '@gobob/ui';
-import { Spice } from '@gobob/icons';
 
 import { Barometer } from './Barometer';
 
@@ -7,23 +6,14 @@ const SeasonInfo = () => {
   return (
     <Flex alignItems='center' direction='column' gap='4xl'>
       <Chip background='grey-500' borderColor='grey-300' size='lg'>
-        Sep 15th - Nov 25th
+        Ends 21st Nov 2024
       </Chip>
-      <Flex alignItems='center' gap='md'>
-        <Spice size='2xl' />
-        <H1 size={{ base: '4xl', md: '6xl' }}>20M Spice</H1>
-      </Flex>
+      <H1 size={{ base: '4xl', md: '6xl' }}>$49.12M DeFi TVL</H1>
       <P align='center' color='grey-50'>
         Season 3 is live from September 15 to November 25, with a huge 20M prize pool at stake. This is your final
         opportunity to gather Spice!
       </P>
-      <Flex alignSelf='normal' direction='column' flex={1} gap='xl'>
-        <Barometer />
-        <Flex alignItems='center' gap='s' justifyContent='center'>
-          <Spice color='grey-50' />
-          <P color='grey-50'>18.25M / 20M Remaining</P>
-        </Flex>
-      </Flex>
+      <Barometer />
     </Flex>
   );
 };
