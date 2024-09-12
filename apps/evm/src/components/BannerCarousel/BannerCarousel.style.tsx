f90e5be3fb80a21866c472f9cc5b667402382a33
Tom Jeatt <hello@tomjeatt.com>
2024-09-12 11:03:13 +0100

    feature: update banners

--- apps/evm/src/components/BannerCarousel/BannerCarousel.style.tsx
+++ apps/evm/src/components/BannerCarousel/BannerCarousel.style.tsx
@@ -3,7 +3,6 @@ import Slider from 'react-slick';
 import styled, { css } from 'styled-components';
 
 import { EcosystemImg } from './EcosystemImg';
-import { IntractImg } from './IntractImg';
 import { OnrampImg } from './OnrampImg';
 
 const StyledCarouselWrapper = styled(Card)`
@@ -88,26 +87,6 @@ const StyledBannerTitle = styled(H1)`
   text-overflow: ellipsis;
 `;
 
-const StyledIntractImg = styled(IntractImg)`
-  ${({ theme }) => {
-    return css`
-      position: absolute;
-      top: 50%;
-      right: 3.275rem;
-      width: 14rem;
-      height: 14rem;
-      transform: translateY(-50%);
-
-      @media ${theme.breakpoints.down('md')} {
-        left: 50%;
-        right: unset;
-        transform: translate(-50%, -50%);
-        opacity: 0.2;
-      }
-    `;
-  }}
-`;
-
 const StyledOnrampImg = styled(OnrampImg)`
   position: absolute;
 
@@ -166,13 +145,32 @@ const StyledBitgetCampaignImg = styled.img`
   }}
 `;
 
+const StyledFusionImg = styled.img`
+  ${({ theme }) => {
+    return css`
+      position: absolute;
+      top: 50%;
+      right: 0;
+      width: 20rem;
+      transform: translateY(-50%);
+
+      @media ${theme.breakpoints.down('md')} {
+        left: 50%;
+        right: unset;
+        transform: translate(-50%, -50%);
+        opacity: 0.2;
+      }
+    `;
+  }}
+`;
+
 export {
   StyledCarouselWrapper,
   StyledSlider,
   StyledBanner,
   StyledEcosystemImg,
+  StyledFusionImg,
   StyledOnrampImg,
-  StyledIntractImg,
   StyledBitgetCampaignImg,
   StyledBannerContent,
   StyledBannerTitle
---
+++ apps/evm/src/components/BannerCarousel/FusionBanner.tsx
@@ -0,0 +1,23 @@
+import { Flex, P } from '@gobob/ui';
+
+import fusionEndImg from '../../assets/fusion-end.png';
+
+import { Banner } from './Banner';
+import { StyledFusionImg } from './BannerCarousel.style';
+import { BannerTitle } from './BannerTitle';
+
+type FusionBannerProps = {
+  onPress?: () => void;
+};
+
+const FusionBanner = ({ onPress }: FusionBannerProps) => (
+  <Banner isPressable direction='column' onPress={onPress}>
+    <Flex alignItems='center'>
+      <BannerTitle>BOB Fusion S02 is Ending Soon!</BannerTitle>
+    </Flex>
+    <P color='grey-50'>Check out the announcement in Discord</P>
+    <StyledFusionImg src={fusionEndImg} />
+  </Banner>
+);
+
+export { FusionBanner };
