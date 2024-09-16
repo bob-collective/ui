6a45b83871e22242ab2026cb691ddf4b9a3fcf19
Slava <slava@interlay.io>
2024-09-16 23:21:08 +0300

    chore: remove unused files

--- apps/evm/src/pages/Stake/constants/steps.ts
+++
@@ -1,7 +0,0 @@
-// l2-processing - relayer did not execute the swap
-// l2-confirmation - relayer executed the swap
-const gatewayDepositSteps = ['btc-confirmation', 'l2-processing', 'l2-confirmation'] as const;
-
-type GatewayDepositSteps = (typeof gatewayDepositSteps)[number];
-
-export type { GatewayDepositSteps };
