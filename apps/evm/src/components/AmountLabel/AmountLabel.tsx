d5ccdae3f9063dc96a6bcd17f96ffa09e2291ee6
Tom Jeatt <hello@tomjeatt.com>
2024-09-17 11:53:08 +0100

    feature: show currency only for staking tokens

--- apps/evm/src/components/AmountLabel/AmountLabel.tsx
+++ apps/evm/src/components/AmountLabel/AmountLabel.tsx
@@ -7,11 +7,12 @@ import { calculateAmountUSD } from '../../utils';
 
 type Props = {
   amount?: CurrencyAmount<Currency>;
+  currencyOnly?: boolean;
 };
 
 type AmountLabelProps = Props;
 
-const AmountLabel = ({ amount }: AmountLabelProps): JSX.Element => {
+const AmountLabel = ({ amount, currencyOnly = false }: AmountLabelProps): JSX.Element => {
   const format = useCurrencyFormatter();
 
   const { getPrice } = usePrices({ baseUrl: import.meta.env.VITE_MARKET_DATA_API });
@@ -23,6 +24,10 @@ const AmountLabel = ({ amount }: AmountLabelProps): JSX.Element => {
 
   if (!amount) {
     return <>-</>;
+  }
+
+  if (currencyOnly) {
+    return <>{amount.currency.symbol}</>;
   }
 
   return (
