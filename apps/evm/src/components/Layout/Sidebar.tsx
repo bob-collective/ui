a062c093880011d60d3d832fdca92ef5b2610011
Slava <slava@interlay.io>
2024-09-19 14:37:26 +0300

    fix(evm): add stake link to sidebar

--- apps/evm/src/components/Layout/Sidebar.tsx
+++ apps/evm/src/components/Layout/Sidebar.tsx
@@ -38,6 +38,7 @@ const Sidebar = ({ isTestnet, isFusion }: SidebarProps): JSX.Element | null => {
             <Nav direction='column' gap='3xl'>
               <NavItem to={RoutesPath.BRIDGE}>{t('navigation.bridge')}</NavItem>
               <NavItem to={RoutesPath.WALLET}>{t('navigation.wallet')}</NavItem>
+              <NavItem to={RoutesPath.STAKE}>{t('navigation.stake')}</NavItem>
               <NavItem to={RoutesPath.FUSION}>{t('navigation.fusion')}</NavItem>
               <NavItem isExternal to='https://safe.gobob.xyz/welcome'>
                 {t('navigation.multisig')}
