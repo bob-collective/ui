1333857bddc1592ae57f05da271205bf4e25a703
Rui Simão <rui.daniel.simao@gmail.com>
2024-09-20 16:37:02 +0100

    feat(evm): add strategies

--- apps/evm/src/pages/Fusion/components/Strategies/Strategies.style.tsx
+++ apps/evm/src/pages/Fusion/components/Strategies/Strategies.style.tsx
@@ -1,4 +1,4 @@
-import { Card, Chip } from '@gobob/ui';
+import { Card, Chip, Flex, H4 } from '@gobob/ui';
 import styled from 'styled-components';
 
 import { QuestRefCodes } from '../../../../utils';
@@ -8,9 +8,14 @@ type StyledCardProps = {
   $questOwner?: QuestRefCodes;
 };
 
-const StyledAvatarWrapper = styled(Card)`
+const StyledBannerWrapper = styled(Flex)`
   position: relative;
-  height: 6.5rem;
+`;
+
+const StyledBanner = styled(Flex)`
+  position: absolute;
+  inset: 0;
+  background: linear-gradient(146.29deg, #5d1b9f 20.01%, #f35d00 104.68%);
 `;
 
 const StyledPrize = styled(Chip)`
@@ -22,6 +27,45 @@ const StyledPrize = styled(Chip)`
 
 const StyledCard = styled(Card)<StyledCardProps>`
   position: relative;
+  height: 7.5rem;
 `;
 
-export { StyledAvatarWrapper, StyledCard, StyledPrize };
+const StyledGrid = styled(Flex)`
+  display: grid;
+
+  @media ${({ theme }) => theme.breakpoints.up('s')} {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+
+    ${StyledCard} {
+      grid-column: span 2 / span 2;
+
+      &:nth-of-type(3) {
+        grid-column-start: 2;
+      }
+    }
+  }
+
+  @media ${({ theme }) => theme.breakpoints.up('md')} {
+    grid-template-columns: 1fr 1fr 1fr;
+
+    ${StyledCard} {
+      grid-column: span 1 / span 1;
+
+      &:nth-of-type(3) {
+        grid-column-start: unset;
+      }
+    }
+  }
+`;
+
+const StyledTitle = styled(H4)`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing('s')};
+  background-color: ${({ theme }) => theme.color('grey-600')};
+  border-radius: ${({ theme }) => theme.rounded('md')};
+  padding: ${({ theme }) => `${theme.spacing('s')} ${theme.spacing('lg')}`};
+`;
+
+export { StyledBannerWrapper, StyledGrid, StyledTitle, StyledBanner, StyledCard, StyledPrize };
--- apps/evm/src/pages/Fusion/components/Strategies/Strategies.tsx
+++ apps/evm/src/pages/Fusion/components/Strategies/Strategies.tsx
@@ -1,15 +1,117 @@
-import { Flex, H2 } from '@gobob/ui';
+import { Flex, H2, Link } from '@gobob/ui';
 
 import { StrategyCard, StrategyCardProps } from './StrategyCard';
+import { StyledGrid } from './Strategies.style';
 
 const btcLstLendingStrat: StrategyCardProps = {
   title: 'BTC LST Lending',
   shortDescription: 'Supply your BTC LSTs into a lending market on BOB',
   longDescription:
-    'Deposit your BTC LSTs into a lending market on BOB and use them as collateral to borrow other assets.'
+    'Deposit your BTC LSTs into a lending market on BOB and use them as collateral to borrow other assets.',
+  steps: [
+    { description: 'One-click stake your BTC and receive BTC LSTs on BOB' },
+    { description: 'Deposit any eligible BTC LST into a lending market on BOB' },
+    {
+      description: 'Optional:',
+      subSteps: [
+        { description: 'Use the deposited BTC LST as collateral to borrow wBTC, tBTC or fBTC' },
+        { description: 'Mint more BTC LSTs with the borrowed asset from step 3a' },
+        { description: 'Deposit into lending as collateral' },
+        { description: 'Repeat steps 3a to 3c multiple times' }
+      ]
+    }
+  ],
+  rewards: [
+    'Lending rate of BTC LSTs on the Lending & Borrowing platform',
+    'Spice rewards (Medium)',
+    'Lending protocol rewards (multiplied by how often users repeat step 3)',
+    'BTC LST points (multiplied by how often users repeat step 3)',
+    'Babylon points (multiplied by how often users repeat step 3)'
+  ]
 };
 
-const strategies = [btcLstLendingStrat, btcLstLendingStrat, btcLstLendingStrat];
+const mintAndLendSatUsd: StrategyCardProps = {
+  title: 'Mint and Lend satUSD',
+  shortDescription: 'Mint satUSD and supply it into a lending market on BOB',
+  longDescription: (
+    <>
+      Deposit your BTC LSTs into{' '}
+      <Link external color='inherit' href='https://app.satoshiprotocol.org/' size='inherit' underlined='always'>
+        Satoshi Protocol
+      </Link>{' '}
+      to mint satUSD and deposit that into a lending market on BOB
+    </>
+  ),
+  steps: [
+    { description: 'One-click stake your BTC and receive BTC LSTs on BOB' },
+    { description: 'Deposit your BTC LSTs into Satoshi Protocol to mint satUSD' },
+    { description: 'Supply the newly minted satUSD into a lending protocol on BOB (insert list/links to dApps)' },
+    {
+      description: 'Optional:',
+      subSteps: [
+        { description: 'Use the deposited satUSD as collateral to borrow any BTC LST' },
+        { description: 'Mint more satUSD with the borrowed asset from step 4a' },
+        { description: 'Deposit into lending as collateral' },
+        { description: 'Repeat steps 4a to 4c multiple times' }
+      ]
+    }
+  ],
+  rewards: [
+    'Lending rate of satUSD',
+    'Spice rewards (Medium)',
+    'Lending protocol rewards (multiplied by how often users repeat step 4)',
+    'Satoshi protocol rewards (multiplied by how often users repeat step 4)',
+    'BTC LST points (multiplied by how often users repeat step 4)',
+    'Babylon points (multiplied by how often users repeat step 4)'
+  ]
+};
+
+const dexLiquidityPovisioning: StrategyCardProps = {
+  title: 'DEX Liquidity Provisioning',
+  shortDescription: 'Provide liquidity into Oku DEX',
+  longDescription: (
+    <>
+      Provide liquidity into any AMM pool on{' '}
+      <Link
+        external
+        color='inherit'
+        href='https://oku.trade/app/bob/?utm_source=BOB'
+        size='inherit'
+        underlined='always'
+      >
+        Oku DEX
+      </Link>{' '}
+      to earn trading fees and incentives
+    </>
+  ),
+  steps: [
+    {
+      description: (
+        <>
+          Follow the steps of{' '}
+          <Link
+            external
+            color='inherit'
+            href='https://docs.oku.trade/home/features/position-manager'
+            size='inherit'
+            underlined='always'
+          >
+            this guide
+          </Link>{' '}
+          to provide liquity into a DEX pool on Oku. Users will need to actively monitor their position, especially if
+          the position has a small range
+        </>
+      )
+    }
+  ],
+  rewards: [
+    'Spice rewards (High)',
+    'BTC LST points (if BTC LSTs are supplied)',
+    'Babylon points (if BTC LSTs are supplied)'
+  ]
+};
+
+const strategies = [btcLstLendingStrat, mintAndLendSatUsd, dexLiquidityPovisioning];
 
 type StrategiesProps = {};
 
@@ -17,11 +119,11 @@ const Strategies = ({}: StrategiesProps) => {
   return (
     <Flex direction='column' gap='3xl' marginTop='8xl'>
       <H2 size='3xl'>Hot Strategies</H2>
-      <Flex direction={{ base: 'column', md: 'row' }} gap='2xl'>
+      <StyledGrid gap='2xl'>
         {strategies.map((strat, idx) => (
           <StrategyCard key={idx} {...strat} />
         ))}
-      </Flex>
+      </StyledGrid>
     </Flex>
   );
 };
