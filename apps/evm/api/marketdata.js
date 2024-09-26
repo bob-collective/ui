8a09c97c6a0f63e306ebae45faccf87679ac89b5
ns212 <nikolai@interlay.io>
2024-09-26 16:55:38 +0000

    api: cache market data

--- apps/evm/api/marketdata.js
+++ apps/evm/api/marketdata.js
@@ -1,8 +1,24 @@
+import { kv } from "@vercel/kv";
+
 export const config = {
   runtime: 'edge'
 };
 
+const headers = {
+  "content-type": "application/json",
+  "cache-control": "public, max-age=120, s-maxage=120, stale-while-revalidate=300, stale-if-error=300"
+}
+
 export default async (request) => {
+  const cache_key = "cg_" + new URL(request.url).searchParams
+  const cached = await kv.get(cache_key)
+  if (cached) {
+    return new Response(JSON.stringify(cached), {
+      status: 200,
+      headers
+    })
+  }
+
   const url = 'https://api.coingecko.com/api/v3/simple/price?' + new URL(request.url).searchParams
   const cgResp = await fetch(url, { headers: { "accept": "application/json" } })
   if (!cgResp.ok) {
@@ -11,11 +27,13 @@ export default async (request) => {
   }
 
   const data = await cgResp.json()
+
+  // cache the data for 120 seconds
+  kv.set(cache_key, data, { ex: 120 })
+    .catch(err => console.error('Unable to cache data', err))
+
   return new Response(JSON.stringify(data), {
     status: 200,
-    headers: {
-      "content-type": "application/json",
-      "cache-control": "public, max-age=120, s-maxage=120, stale-while-revalidate=300, stale-if-error=300"
-    }
+    headers
   })
 };
