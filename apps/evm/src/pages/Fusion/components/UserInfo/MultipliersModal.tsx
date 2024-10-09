3052396d23781efc79a2dd5bc40384f98ce0a4ed
Tom Jeatt <hello@tomjeatt.com>
2024-10-09 12:23:18 +0100

    chore: user info copy

--- apps/evm/src/pages/Fusion/components/UserInfo/MultipliersModal.tsx
+++ apps/evm/src/pages/Fusion/components/UserInfo/MultipliersModal.tsx
@@ -1,6 +1,7 @@
 import { Flex, H3, Modal, ModalBody, ModalHeader, ModalProps, P, Skeleton, Span, Table, useLocale } from '@gobob/ui';
 import { ReactNode, useCallback, useId } from 'react';
 import { Address, isAddressEqual } from 'viem';
+import { useTranslation } from 'react-i18next';
 
 import { useGetTokensInfo } from '../../hooks';
 import { TokenInfo } from '../../../../utils';
@@ -32,27 +33,6 @@ type TableRow = {
   [TableColumns.DEX]: ReactNode;
 };
 
-const columns = [
-  { id: TableColumns.ASSET, width: 160, name: 'Asset' },
-  { id: TableColumns.HOLDING, name: 'Holding' },
-  {
-    id: TableColumns.LENDING,
-    name: (
-      <Span noWrap size='inherit' weight='inherit'>
-        Lending, Restaking, CDP *
-      </Span>
-    )
-  },
-  {
-    id: TableColumns.DEX,
-    name: (
-      <Span noWrap size='inherit' weight='inherit'>
-        DEX *
-      </Span>
-    )
-  }
-];
-
 type Props = {};
 
 type InheritAttrs = Omit<ModalProps, keyof Props | 'children'>;
@@ -61,12 +41,34 @@ type MultipliersModalProps = Props & InheritAttrs;
 
 const MultipliersModal = (props: MultipliersModalProps): JSX.Element => {
   const { locale } = useLocale();
+  const { t } = useTranslation();
 
   const { data: tokensInfo } = useGetTokensInfo();
 
   const featuredAssetId = useId();
   const yieldAssetId = useId();
   const baseAssetId = useId();
+
+  const columns = [
+    { id: TableColumns.ASSET, width: 160, name: t('fusion.userInfo.multipliersModal.columns.asset') },
+    { id: TableColumns.HOLDING, name: t('fusion.userInfo.multipliersModal.columns.holding') },
+    {
+      id: TableColumns.LENDING,
+      name: (
+        <Span noWrap size='inherit' weight='inherit'>
+          {t('fusion.userInfo.multipliersModal.columns.lending')}
+        </Span>
+      )
+    },
+    {
+      id: TableColumns.DEX,
+      name: (
+        <Span noWrap size='inherit' weight='inherit'>
+          {t('fusion.userInfo.multipliersModal.columns.dex')}
+        </Span>
+      )
+    }
+  ];
 
   const getRow = useCallback(
     (item: Pick<TokenInfo, 'symbol' | 'multiplier'>, idx: number): TableRow => ({
@@ -126,32 +128,30 @@ const MultipliersModal = (props: MultipliersModalProps): JSX.Element => {
   return (
     <Modal {...props} size='xl'>
       <ModalHeader showDivider align='start'>
-        Multipliers
+        {t('fusion.userInfo.multipliersModal.title')}
       </ModalHeader>
       <ModalBody gap='2xl' padding='even'>
-        <P color='grey-50'>
-          Deploy high priority assets into high priority DeFi protocols to maximize your Spice harvest.
-        </P>
+        <P color='grey-50'>{t('fusion.userInfo.multipliersModal.description')}</P>
         <Flex direction='column' gap='md'>
           <H3 id={featuredAssetId} size='md'>
-            Featured Assets
+            {t('fusion.userInfo.multipliersModal.sections.featured')}
           </H3>
           <Table removeWrapper aria-labelledby={featuredAssetId} columns={columns} rows={featuredAssetsRows} />
         </Flex>
         <Flex direction='column' gap='md'>
           <H3 id={yieldAssetId} size='md'>
-            Yield Assets
+            {t('fusion.userInfo.multipliersModal.sections.yield')}
           </H3>
           <Table removeWrapper aria-labelledby={yieldAssetId} columns={columns} rows={yieldAssetsRows} />
         </Flex>
         <Flex direction='column' gap='md'>
           <H3 id={baseAssetId} size='md'>
-            Base Assets
+            {t('fusion.userInfo.multipliersModal.sections.base')}
           </H3>
           <Table removeWrapper aria-labelledby={baseAssetId} columns={columns} rows={baseAssetsRows} />
         </Flex>
         <P align='center' color='grey-50' size='xs'>
-          * The multipliers displayed are subject to change based on the payout structure of the respective projects.
+          {t('fusion.userInfo.multipliersModal.disclaimer')}
         </P>
       </ModalBody>
     </Modal>
