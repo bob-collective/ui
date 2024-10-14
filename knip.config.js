7620c8dcdc60d54afd6efdfb14af07be9bdfed3b
Slava <slava@interlay.io>
2024-10-14 15:00:33 +0300

    chore: update knip

--- knip.config.js
+++ knip.config.js
@@ -6,12 +6,13 @@ const config = {
     'apps/evm/api/**',
     'apps/evm-next/api/**',
     'apps/evm-next/src/i18n/**',
+    'apps/evm-next/src/connect-ui/**',
     'apps/evm-next/sentry.ts',
     'packages/currency/src/constants.ts',
     'packages/sats-wagmi/package.json',
     '.storybook/vite.config.ts'
   ],
-  ignoreDependencies: ['@lingui/swc-plugin', '@lingui/loader']
+  ignoreDependencies: ['@lingui/swc-plugin', '@lingui/loader', 'sharp']
 };
 
 export default config;
