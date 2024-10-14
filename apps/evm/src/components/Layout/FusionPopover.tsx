021244c6e5dece3f64609245f85bc56dcad60812
Tom Jeatt <hello@tomjeatt.com>
2024-10-14 12:10:56 +0100

    feature: add popover translation

--- apps/evm/src/components/Layout/FusionPopover.tsx
+++ apps/evm/src/components/Layout/FusionPopover.tsx
@@ -12,6 +12,7 @@ import {
 } from '@gobob/ui';
 import { Spice } from '@gobob/icons';
 import { useFocusRing } from '@react-aria/focus';
+import { useTranslation } from 'react-i18next';
 
 import { useGetUser, useHaltedLockedTokens, useLockedTokens } from '../../hooks';
 import { WithdrawAlert } from '../WithdrawAlert';
@@ -23,6 +24,7 @@ import { StyledChip, StyledContentWrapper, StyledHarvestCard, StyledOpacityOverl
 const FusionPopover = (): JSX.Element | null => {
   const { data: user } = useGetUser();
   const { locale } = useLocale();
+  const { t } = useTranslation();
 
   useLockedTokens();
   useHaltedLockedTokens();
@@ -50,38 +52,38 @@ const FusionPopover = (): JSX.Element | null => {
           <PopoverHeader showDivider={false}>Your Spice Harvest Overview</PopoverHeader>
           <PopoverBody gap='md' padding='even'>
             <P color='grey-50' size='s'>
-              Season 3 (Ongoing)
+              {t('fusion.popover.season3')}
             </P>
             <StyledHarvestCard rounded='md'>
               <StyledOpacityOverlay />
               <StyledContentWrapper direction='column'>
                 <P color='grey-50' size='s'>
-                  Current Harvest
+                  {t('fusion.popover.currentHarvest')}
                 </P>
                 <SpiceAmount amount={season3TotalPoints} />
               </StyledContentWrapper>
             </StyledHarvestCard>
             <Card background='grey-500' rounded='lg'>
               <P color='grey-50' size='s'>
-                Current Leaderboard Rank
+                {t('fusion.popover.currentLeaderboardRank')}
               </P>
               <P>#{season3leaderboardData.group_rank}</P>
             </Card>
             <>
               <Divider marginY='xs' />
               <P color='grey-50' size='s'>
-                Season 1 & 2 (Completed)
+                {t('fusion.popover.season1and2')}
               </P>
               <Card background='grey-500' rounded='lg'>
                 <P color='grey-50' size='s'>
-                  Total Harvest
+                  {t('fusion.popover.totalHarvest')}
                 </P>
                 <SpiceAmount amount={user.leaderboardRank?.total_points || 0} />
               </Card>
               {user.leaderboardRank?.rank && (
                 <Card background='grey-500' rounded='lg'>
                   <P color='grey-50' size='s'>
-                    Season 2 Final Rank
+                    {t('fusion.popover.season2FinalRank')}
                   </P>
                   <P>#{user.leaderboardRank?.rank}</P>
                 </Card>
