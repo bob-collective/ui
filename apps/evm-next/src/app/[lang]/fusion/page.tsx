3f8d5412bb21b4b696a3f7688ebd8ecaf81bb55f
Slava <slava@interlay.io>
2024-10-15 11:43:20 +0300

    chore: add page title translations

--- apps/evm-next/src/app/[lang]/fusion/page.tsx
+++ apps/evm-next/src/app/[lang]/fusion/page.tsx
@@ -1,11 +1,18 @@
 import { Metadata } from 'next';
+import { t } from '@lingui/macro';
 
 import { Fusion } from './Fusion';
 
 import { withLinguiPage } from '@/i18n/withLigui';
+import { getI18nInstance } from '@/i18n/appRouterI18n';
+import { PageLangParam } from '@/i18n/withLigui';
 
-export const metadata: Metadata = {
-  title: 'BOB | Fusion'
-};
+export function generateMetadata({ params }: PageLangParam): Metadata {
+  const i18n = getI18nInstance(params.lang);
+
+  return {
+    title: `BOB | ${t(i18n)`Fusion`}`
+  };
+}
 
 export default withLinguiPage(Fusion);
