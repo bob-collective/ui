866f8c3854852459070ea20626c2a98a8d38f0f9
Slava <slava@interlay.io>
2024-10-17 15:28:56 +0300

    chore: update knip ignore

--- knip.config.js
+++ knip.config.js
@@ -4,10 +4,9 @@ const config = {
   ignoreBinaries: ['synpress:run'],
   ignore: [
     'apps/evm/api/**',
-    'apps/evm-next/api/**',
-    'apps/evm-next/src/i18n/**',
-    'apps/evm-next/src/connect-ui/**',
-    'apps/evm-next/sentry.ts',
+    'apps/evm/src/i18n/**',
+    'apps/evm/src/connect-ui/**',
+    'apps/evm/sentry.ts',
     'packages/currency/src/constants.ts',
     'packages/sats-wagmi/package.json',
     '.storybook/vite.config.ts'
