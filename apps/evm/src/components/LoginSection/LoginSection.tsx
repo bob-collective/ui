8da13cd44ed3445cd1bfe179a55ac94b32875b26
Tom Jeatt <hello@tomjeatt.com>
2024-10-18 16:14:17 +0100

    fix: revert unneeded change

--- apps/evm/src/components/LoginSection/LoginSection.tsx
+++ apps/evm/src/components/LoginSection/LoginSection.tsx
@@ -7,16 +7,14 @@ type LoginSectionProps = FlexProps;
 
 const LoginSection = ({ direction = { base: 'column', md: 'row' }, ...props }: LoginSectionProps): JSX.Element => {
   return (
-    <>
-      <Flex alignItems='center' direction={direction} gap='xs' justifyContent='center' {...props}>
-        <P size='s' weight='bold'>
-          <Trans>Already harvesting?</Trans>
-        </P>
-        <LoginButton color='primary' size='s' variant='ghost'>
-          <Trans>Login with wallet</Trans>
-        </LoginButton>
-      </Flex>
-    </>
+    <Flex alignItems='center' direction={direction} gap='xs' justifyContent='center' {...props}>
+      <P size='s' weight='bold'>
+        <Trans>Already harvesting?</Trans>
+      </P>
+      <LoginButton color='primary' size='s' variant='ghost'>
+        <Trans>Login with wallet</Trans>
+      </LoginButton>
+    </Flex>
   );
 };
 
