bc8c5a9955685a429e13f66d35908bd0e6c1cdf1
Tom Jeatt <hello@tomjeatt.com>
2024-10-18 16:15:05 +0100

    fix: revert change

--- apps/evm/src/components/LoginSection/LoginSection.tsx
+++ apps/evm/src/components/LoginSection/LoginSection.tsx
@@ -5,17 +5,15 @@ import { LoginButton } from '../LoginButton';
 
 type LoginSectionProps = FlexProps;
 
-const LoginSection = ({ direction = { base: 'column', md: 'row' }, ...props }: LoginSectionProps): JSX.Element => {
-  return (
-    <Flex alignItems='center' direction={direction} gap='xs' justifyContent='center' {...props}>
-      <P size='s' weight='bold'>
-        <Trans>Already harvesting?</Trans>
-      </P>
-      <LoginButton color='primary' size='s' variant='ghost'>
-        <Trans>Login with wallet</Trans>
-      </LoginButton>
-    </Flex>
-  );
-};
+const LoginSection = ({ direction = { base: 'column', md: 'row' }, ...props }: LoginSectionProps): JSX.Element => (
+  <Flex alignItems='center' direction={direction} gap='xs' justifyContent='center' {...props}>
+    <P size='s' weight='bold'>
+      <Trans>Already harvesting?</Trans>
+    </P>
+    <LoginButton color='primary' size='s' variant='ghost'>
+      <Trans>Login with wallet</Trans>
+    </LoginButton>
+  </Flex>
+);
 
 export { LoginSection };
