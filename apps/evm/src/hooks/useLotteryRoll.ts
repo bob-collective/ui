319bd6a4e9939a15da1898eb5110981cda8e4fe7
Slava <slava@interlay.io>
2024-10-23 19:10:45 +0300

    chore: add mocked backend calls

--- apps/evm/src/hooks/useLotteryRoll.ts
+++ apps/evm/src/hooks/useLotteryRoll.ts
@@ -1,18 +1,42 @@
 import { useMutation, UseMutationOptions, useQueryClient } from '@gobob/react-query';
+import { useState } from 'react';
+import { Address } from 'viem';
 
 import { fusionKeys } from '@/lib/react-query';
-import { apiClient, LotteryRoll } from '@/utils';
-import { Address } from 'viem';
+import { LotteryRoll } from '@/utils';
 
 const useLotteryRoll = (
   address: Address | undefined,
   props: Omit<UseMutationOptions<LotteryRoll, Error, void, string[]>, 'mutationFn'> = {}
 ) => {
   const queryClient = useQueryClient();
+  const [state, setState] = useState(3);
 
   return useMutation({
-    mutationFn: () => apiClient.lotteryRoll(),
+    // mutationFn: () => apiClient.lotteryRoll(),
+    mutationFn: async () => {
+      if (state === 0) {
+        return {
+          rollsRemaining: 0, // How many rolls are left
+          votesRemaining: 0, // How many votes are left, if above zero user is missing out on rolls as 1 vote = 1 roll
+          running: true, // Is the lottery running, if their is no current voting round, the user cannot roll
+          pointsMissing: 10500, // The user has to have 10500 points to roll, this is the number of points they are missing
+          packageId: 1, // The id of the package the user won
+          prize: state % 2 ? 10500 : 0 // The number of spice the user won
+        };
+      }
+
+      return {
+        rollsRemaining: state, // How many rolls are left
+        votesRemaining: 0, // How many votes are left, if above zero user is missing out on rolls as 1 vote = 1 roll
+        running: true, // Is the lottery running, if their is no current voting round, the user cannot roll
+        pointsMissing: 10500, // The user has to have 10500 points to roll, this is the number of points they are missing
+        packageId: 1, // The id of the package the user won
+        prize: state % 2 ? 10500 : 0 // The number of spice the user won
+      };
+    },
     onSuccess: () => {
+      setState((val) => val - 1);
       queryClient.refetchQueries({ queryKey: fusionKeys.lotteryStats(address) });
     },
     ...props
--- apps/evm/src/hooks/useLotteryStats.ts
+++ apps/evm/src/hooks/useLotteryStats.ts
@@ -1,8 +1,9 @@
 import { useQuery, UseQueryOptions } from '@gobob/react-query';
 import { Address } from 'viem';
+import { useState } from 'react';
 
 import { fusionKeys } from '@/lib/react-query';
-import { apiClient, LotteryStats } from '@/utils';
+import { LotteryStats } from '@/utils';
 
 const useLotteryStats = (
   address: Address | undefined,
@@ -11,9 +12,30 @@ const useLotteryStats = (
     'queryKey' | 'queryFn' | 'refetchInterval' | 'enabled'
   > = {}
 ) => {
+  const [state, setState] = useState(3);
+
   return useQuery({
     queryKey: fusionKeys.lotteryStats(address),
-    queryFn: () => apiClient.getLotteryStats(),
+    // queryFn: () => apiClient.getLotteryStats(),
+    queryFn: async () => {
+      setState((val) => val - 1);
+
+      if (state === 0) {
+        return {
+          pointsMissing: 0,
+          rollsRemaining: 0,
+          running: true,
+          votesRemaining: 2
+        };
+      }
+
+      return {
+        pointsMissing: 0,
+        rollsRemaining: state,
+        running: true,
+        votesRemaining: 0
+      };
+    },
     refetchOnWindowFocus: false,
     refetchOnMount: false,
     enabled: Boolean(address),
