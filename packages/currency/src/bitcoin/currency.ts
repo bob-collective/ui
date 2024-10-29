b5765c48a7feff8dda923c41a6e56e29169b24b5
Rui Simão <rui.daniel.simao@gmail.com>
2024-10-29 09:23:03 +0000

    fix(evm): finalizing

--- packages/currency/src/bitcoin/currency.ts
+++ packages/currency/src/bitcoin/currency.ts
@@ -54,32 +54,3 @@ export class Bitcoin extends BtcCurrency {
     return other instanceof BtcCurrency && other.isNative;
   }
 }
-
-/**
- * A currency is any fungible financial instrument, including Ether, all ERC20 tokens, and other chain-native currencies
- */
-export class BRC20Token extends BtcCurrency {
-  /**
-   * Returns whether the currency is native to the chain and must be wrapped (e.g. Ether)
-   */
-  public readonly isNative: false = false as const;
-
-  /**
-   * Returns whether the currency is a token that is usable in PancakeSwap without wrapping
-   */
-  public readonly isToken: true = true as const;
-
-  /**
-   * Constructs an instance of the base class `BaseCurrency`.
-   * @param decimals decimals of the currency
-   * @param symbol symbol of the currency
-   * @param name of the currency
-   */
-  public constructor(decimals: number, symbol: string, name?: string) {
-    super(decimals, symbol, name);
-  }
-
-  public equals(other: Currency): boolean {
-    return other instanceof BtcCurrency && other.isToken && this.symbol === other.symbol;
-  }
-}
