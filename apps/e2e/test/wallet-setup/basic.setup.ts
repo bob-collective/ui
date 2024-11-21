f0cb597b82fc9430fd89bf1a20f58084aea1e31a
Slava <slava@interlay.io>
2024-11-21 18:06:58 +0300

    chore: update seed and password vars

--- apps/e2e/test/wallet-setup/basic.setup.ts
+++ apps/e2e/test/wallet-setup/basic.setup.ts
@@ -1,8 +1,8 @@
 import { defineWalletSetup } from '@synthetixio/synpress';
 import { MetaMask } from '@synthetixio/synpress/playwright';
 
-const SEED_PHRASE = 'test test test test test test test test test test test junk';
-const PASSWORD = 'SynpressIsAwesomeNow!!!';
+const SEED_PHRASE = process.env.SEED_PHRASE;
+const PASSWORD = process.env.WALLET_PASSWORD;
 
 export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
   const metamask = new MetaMask(context, walletPage, PASSWORD);
