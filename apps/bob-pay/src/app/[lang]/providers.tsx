2541095810a1891f955f73c3f698b94dbc4ba284
Rui Simão <rui.daniel.simao@gmail.com>
2024-11-26 17:08:43 +0000

    fix: add bob-pay

--- apps/bob-pay/src/app/[lang]/providers.tsx
+++ apps/bob-pay/src/app/[lang]/providers.tsx
@@ -4,15 +4,15 @@ import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
 import { ZeroDevSmartWalletConnectors } from '@dynamic-labs/ethereum-aa';
 import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
 import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
-import { QueryClientProvider } from '@gobob/react-query';
-import { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@gobob/react-query';
+import { PropsWithChildren, useState } from 'react';
 import { bob, bobSepolia } from 'viem/chains';
 import { createConfig, http, WagmiProvider } from 'wagmi';
 
 import { NestedProviders } from './nested-providers';
 
-import { queryClient } from '@/lib/react-query';
 import { BalanceProvider } from '@/providers';
+import { FetchError } from '@/types/fetch';
 
 const config = createConfig({
   chains: [bob, bobSepolia],
@@ -24,6 +24,33 @@ const config = createConfig({
 });
 
 export function Providers({ children }: PropsWithChildren) {
+  // Instead do this, which ensures each request has its own cache:
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // Ideally, these default values should never be used.
+            // Each query should set its own `staleTime` and `gcTime` depending on how often the data is expected to change,
+            // and how important it is to keep the data fresh every time a component mounts.
+            staleTime: 15 * 1000,
+            gcTime: 24 * 60 * 60 * 1000,
+            // Retry once, only if the error is a 500 fetch error.
+            refetchOnWindowFocus: false,
+            retry: (failureCount, error): boolean => {
+              if (failureCount >= 2) return false;
+
+              if (error instanceof FetchError && error.status === 500) {
+                return true; // Retry on server error
+              }
+
+              return false;
+            }
+          }
+        }
+      })
+  );
+
   return (
     <DynamicContextProvider
       settings={{
