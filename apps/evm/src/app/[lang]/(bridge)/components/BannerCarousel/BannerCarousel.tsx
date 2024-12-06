97a25c77102092802d11ad6656d4223a31aa10e9
Tom Jeatt <hello@tomjeatt.com>
2024-12-06 09:56:22 +0000

    chore: remove banners

--- apps/evm/src/app/[lang]/(bridge)/components/BannerCarousel/BannerCarousel.tsx
+++ apps/evm/src/app/[lang]/(bridge)/components/BannerCarousel/BannerCarousel.tsx
@@ -3,33 +3,19 @@
 import { useMediaQuery } from '@gobob/ui';
 import { t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
-import { useParams, useRouter } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
 import 'react-multi-carousel/lib/styles.css';
 import { useTheme } from 'styled-components';
-import { useIsClient, useSessionStorage } from 'usehooks-ts';
 
 import { StyledCarousel, StyledCarouselWrapper } from './BannerCarousel.style';
 import { FusionBanner } from './FusionBanner';
-import { OnrampBanner } from './OnrampBanner';
 import { XBanner } from './XBanner';
 import { HybridL2Banner } from './HybridL2Banner';
-import { OKXCryptopediaBanner } from './OKXCryptopediaBanner';
-
-import { RoutesPath } from '@/constants';
-import { SessionStorageKey } from '@/types';
 
 const BannerCarousel = () => {
   const { i18n } = useLingui();
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up('s'));
-  const router = useRouter();
-  const params = useParams();
-  const isClient = useIsClient();
-
-  const [, setBridgeToBtc] = useSessionStorage(SessionStorageKey.BRIDGE_TO_BTC, false, {
-    initializeWithValue: isClient
-  });
 
   const responsive = useMemo(
     () => ({
@@ -52,15 +38,6 @@ const BannerCarousel = () => {
     [theme.breakpoints]
   );
 
-  const onPressOnrampBanner = useCallback(
-    () => {
-      setBridgeToBtc(true);
-      router.push(`/${params?.lang}${RoutesPath.BRIDGE}`);
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
-
   const onPressFusionBanner = useCallback(
     () => window.open('https://blog.gobob.xyz/posts/bob-fusion-the-final-season', '_blank', 'noreferrer'),
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -75,12 +52,6 @@ const BannerCarousel = () => {
 
   const onPressHybridL2Banner = useCallback(
     () => window.open('https://blog.gobob.xyz/posts/the-hybrid-l2-paper', '_blank', 'noreferrer'),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
-
-  const onPressOKXCryptopediaBanner = useCallback(
-    () => window.open('https://www.okx.com/web3/discover/cryptopedia/event/bob', '_blank', 'noreferrer'),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
@@ -103,11 +74,9 @@ const BannerCarousel = () => {
         swipeable={false}
         transitionDuration={500}
       >
-        <OKXCryptopediaBanner onPress={onPressOKXCryptopediaBanner} />
         <HybridL2Banner onPress={onPressHybridL2Banner} />
         <XBanner onPress={onPressXBanner} />
         <FusionBanner onPress={onPressFusionBanner} />
-        <OnrampBanner onPress={onPressOnrampBanner} />
       </StyledCarousel>
     </StyledCarouselWrapper>
   );
