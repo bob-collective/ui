59360703551487a35f3de14f8a80be5a9c237ab9
Rui Simão <rui.daniel.simao@gmail.com>
2024-12-06 17:52:03 +0000

    feat(evm): final

--- apps/evm/src/app/[lang]/(bridge)/utils/stakeData.tsx
+++ apps/evm/src/app/[lang]/(bridge)/utils/stakeData.tsx
@@ -1,5 +1,3 @@
-import { Alert, Flex, P } from '@gobob/ui';
-import { Trans } from '@lingui/macro';
 import { ValueOf } from 'viem';
 
 enum Incentive {
@@ -14,102 +12,69 @@ enum Incentive {
 
 type StakingInfo = Record<string, ValueOf<typeof stakingInfo> | undefined>;
 
+const babylonWithdrawWarning = 'Babylon does not yet support withdrawals.';
+
 const stakingInfo = {
   'bedrock-unibtc': {
     strategy: 'Liquid Staking Bedrock-Babylon',
     protocol: 'Bedrock',
     incentives: [Incentive.bedrock, Incentive.babylon],
     tvl: '-',
-    about: (
-      <Flex direction='column' gap='md'>
-        <P>
-          <Trans>Stake BTC into Babylon via Bedrock and receive liquid staking token uniBTC.</Trans>
-        </P>
-        <Alert status='warning'>
-          <Trans>Attention: Babylon does not yet support withdrawals.</Trans>
-        </Alert>
-      </Flex>
-    ),
+    about: 'Stake BTC into Babylon via Bedrock and receive liquid staking token uniBTC.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'uniBTC',
     securityReview: 'https://www.bitcoinlayers.org/infrastructure/bedrock',
-    website: 'https://app.bedrock.technology/unibtc'
+    website: 'https://app.bedrock.technology/unibtc',
+    warningMessage: babylonWithdrawWarning
   },
   'solv-solvbtcbbn': {
     strategy: 'Liquid Staking Solv-Babylon',
     protocol: 'Solv',
     incentives: [Incentive.solv, Incentive.babylon],
     tvl: '-',
-    about: (
-      <Flex direction='column' gap='md'>
-        <P>
-          <Trans>Stake BTC into Babylon via Solv Protocol and receive liquid staking token solvBTC.BBN.</Trans>
-        </P>
-        <Alert status='warning'>
-          <Trans>Attention: Babylon does not yet support withdrawals.</Trans>
-        </Alert>
-      </Flex>
-    ),
+    about: 'Stake BTC into Babylon via Solv Protocol and receive liquid staking token solvBTC.BBN.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'SolvBTC.BBN',
     securityReview: 'https://www.bitcoinlayers.org/infrastructure/solvlst',
-    website: 'https://app.solv.finance/babylon?network=bob'
+    website: 'https://app.solv.finance/babylon?network=bob',
+    warningMessage: babylonWithdrawWarning
   },
   'pell-solvbtcbbn': {
     strategy: 'Restaking Pell-SolvBTC-Babylon',
     protocol: 'Pell',
     incentives: [Incentive.pell, Incentive.solv, Incentive.babylon],
     tvl: '-',
-    about: (
-      <Flex direction='column' gap='md'>
-        <P>
-          <Trans>
-            Stake BTC into Babylon via Solv Protocol, get solvBTC.BBN liquid staking token, and deposit into Pell.
-          </Trans>
-        </P>
-        <Alert status='warning'>
-          <Trans>Attention: Babylon does not yet support withdrawals.</Trans>
-        </Alert>
-      </Flex>
-    ),
+    about: 'Stake BTC into Babylon via Solv Protocol, get solvBTC.BBN liquid staking token, and deposit into Pell.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'SolvBTC.BBN',
     securityReview: '',
-    website: 'https://app.pell.network/restake'
+    website: 'https://app.pell.network/restake',
+    warningMessage: babylonWithdrawWarning
   },
   'pell-unibtc': {
     strategy: 'Restaking Pell-uniBTC-Babylon',
     protocol: 'Pell',
     incentives: [Incentive.pell, Incentive.bedrock, Incentive.babylon],
     tvl: '-',
-    about: (
-      <Flex direction='column' gap='md'>
-        <P>
-          <Trans>
-            Stake BTC into Babylon via Bedrock, get uniBTC liquid staking token, and deposit into Pell restaking.
-          </Trans>
-        </P>
-        <Alert status='warning'>
-          <Trans>Attention: Babylon does not yet support withdrawals.</Trans>
-        </Alert>
-      </Flex>
-    ),
+    about: 'Stake BTC into Babylon via Bedrock, get uniBTC liquid staking token, and deposit into Pell restaking.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'uniBTC',
     securityReview: '',
-    website: 'https://app.pell.network/restake'
+    website: 'https://app.pell.network/restake',
+    warningMessage: babylonWithdrawWarning
   },
   'segment-tbtc': {
     strategy: 'Lending Segment-tBTC',
     protocol: 'Segment',
     incentives: [Incentive.segment, Incentive.supply],
     tvl: '-',
-    about: (
-      <P>
-        <Trans>Lend out tBTC on Segment.</Trans>
-      </P>
-    ),
+    about: 'Lend out tBTC on Segment.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'tBTC',
     securityReview: 'https://www.bitcoinlayers.org/infrastructure/tbtc',
     website: 'https://app.segment.finance'
@@ -119,12 +84,9 @@ const stakingInfo = {
     protocol: 'Segment',
     incentives: [Incentive.segment, Incentive.supply],
     tvl: '-',
-    about: (
-      <P>
-        <Trans>Lend out wBTC on Segment.</Trans>
-      </P>
-    ),
+    about: 'Lend out wBTC on Segment.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'wBTC',
     securityReview: 'https://www.bitcoinlayers.org/infrastructure/wbtc',
     website: 'https://app.segment.finance'
@@ -134,44 +96,27 @@ const stakingInfo = {
     protocol: 'Segment',
     incentives: [Incentive.segment, Incentive.solv, Incentive.babylon, Incentive.supply],
     tvl: '-',
-    about: (
-      <Flex direction='column' gap='md'>
-        <P>
-          <Trans>
-            Stake BTC into Babylon via Solv Protocol, get solvBTC.BBN liquid staking token, and lend it out on Segment.
-          </Trans>
-        </P>
-        <Alert status='warning'>
-          <Trans>Attention: Babylon does not yet support withdrawals.</Trans>
-        </Alert>
-      </Flex>
-    ),
+    about:
+      'Stake BTC into Babylon via Solv Protocol, get solvBTC.BBN liquid staking token, and lend it out on Segment.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'seUNIBTC',
     securityReview: '',
-    website: 'https://app.segment.finance'
+    website: 'https://app.segment.finance',
+    warningMessage: babylonWithdrawWarning
   },
   'segment-seunibtc': {
     strategy: 'Staked Lending Segment-uniBTC-Babylon',
     protocol: 'Segment',
     incentives: [Incentive.segment, Incentive.bedrock, Incentive.babylon, Incentive.supply],
     tvl: '-',
-    about: (
-      <Flex direction='column' gap='md'>
-        <P>
-          <Trans>
-            Stake BTC into Babylon via Bedrock, get uniBTC liquid staking token, and lend it out on Segment.
-          </Trans>
-        </P>
-        <Alert status='warning'>
-          <Trans>Attention: Babylon does not yet support withdrawals.</Trans>
-        </Alert>
-      </Flex>
-    ),
+    about: 'Stake BTC into Babylon via Bedrock, get uniBTC liquid staking token, and lend it out on Segment.',
     inputToken: 'BTC',
+    inputTokenLogoUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png',
     outputToken: 'seUNIBTC',
     securityReview: '',
-    website: 'https://app.segment.finance'
+    website: 'https://app.segment.finance',
+    warningMessage: babylonWithdrawWarning
   }
 } as const;
 
