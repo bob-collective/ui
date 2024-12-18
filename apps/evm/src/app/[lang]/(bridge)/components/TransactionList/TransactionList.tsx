bcf665ee572cc757563c740d566829d4fca9d708
Slava <slava@interlay.io>
2024-12-18 14:30:19 +0300

    chore: add virtual list

--- apps/evm/src/app/[lang]/(bridge)/components/TransactionList/TransactionList.tsx
+++ apps/evm/src/app/[lang]/(bridge)/components/TransactionList/TransactionList.tsx
@@ -1,8 +1,9 @@
 import { CardProps, Divider, Flex, H2, Link, P, Spinner } from '@gobob/ui';
 import { Trans } from '@lingui/macro';
-import { Fragment, useMemo } from 'react';
+import { useMemo, useRef } from 'react';
 import { useIsClient } from 'usehooks-ts';
 import { useAccount } from 'wagmi';
+import { useVirtualizer } from '@tanstack/react-virtual';
 
 import { TransactionItem } from './TransactionItem';
 import {
@@ -36,6 +37,19 @@ const TransactionList = ({
   txPendingUserAction,
   ...props
 }: TransactionListProps): JSX.Element => {
+  // The scrollable element for your list
+  const parentRef = useRef(null);
+
+  // The virtualizer
+  const rowVirtualizer = useVirtualizer({
+    count: data?.length || 0,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 102,
+    paddingStart: 16,
+    paddingEnd: 16,
+    gap: 16
+  });
+
   const { address, chain } = useAccount();
   const isClient = useIsClient();
 
@@ -58,6 +72,8 @@ const TransactionList = ({
   const txsUrl = address ? `${explorerUrl}/address/${address}` : `${explorerUrl}`;
   const hasData = !!data?.length;
 
+  const dividerStyle = { marginTop: '1rem' };
+
   return (
     <StyledSection gap='xl' paddingX='4xl' paddingY='3xl' {...props}>
       <H2 size='md'>{title}</H2>
@@ -68,7 +84,6 @@ const TransactionList = ({
           flex={1}
           gap='xl'
           justifyContent={isInitialLoading || !hasData ? 'center' : undefined}
-          paddingY='xl'
         >
           {!isClient || isInitialLoading ? (
             <Flex alignItems='center' gap='md' justifyContent='center' style={{ height: '100%' }}>
@@ -80,16 +95,42 @@ const TransactionList = ({
           ) : (
             <>
               {hasData ? (
-                data.map((transaction, idx) => (
-                  <Fragment key={idx}>
-                    <TransactionItem
-                      data={transaction}
-                      onProveSuccess={onProveSuccess}
-                      onRelaySuccess={onRelaySuccess}
-                    />
-                    {idx < data.length - 1 && <Divider />}
-                  </Fragment>
-                ))
+                <div
+                  ref={parentRef}
+                  style={{
+                    height: `610px`,
+                    overflow: 'auto'
+                  }}
+                >
+                  <div
+                    style={{
+                      height: `${rowVirtualizer.getTotalSize()}px`,
+                      width: '100%',
+                      position: 'relative'
+                    }}
+                  >
+                    {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+                      <div
+                        key={virtualItem.key}
+                        style={{
+                          position: 'absolute',
+                          top: 0,
+                          left: 0,
+                          width: '100%',
+                          height: `${virtualItem.size}px`,
+                          transform: `translateY(${virtualItem.start}px)`
+                        }}
+                      >
+                        <TransactionItem
+                          data={data[virtualItem.index]!}
+                          onProveSuccess={onProveSuccess}
+                          onRelaySuccess={onRelaySuccess}
+                        />
+                        {virtualItem.index < data.length - 1 && <Divider style={dividerStyle} />}
+                      </div>
+                    ))}
+                  </div>
+                </div>
               ) : (
                 <Flex alignItems='center' gap='md' justifyContent='center' style={{ height: '100%' }}>
                   <P align='center' size='xs'>
