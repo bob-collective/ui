2208ba7a431060b09019b63dcc1592b01dbc8f1f
Rui Simão <rui.daniel.simao@gmail.com>
2025-01-03 12:29:55 +0000

    fix(evm): strategies info

--- apps/evm/src/app/[lang]/(bridge)/stake/constants/strategies.tsx
+++ apps/evm/src/app/[lang]/(bridge)/stake/constants/strategies.tsx
@@ -1,7 +1,7 @@
 import { Trans } from '@lingui/macro';
 import { ReactNode } from 'react';
 
-enum Incentive {
+enum StrategyIncentive {
   spice,
   pell,
   bedrock,
@@ -11,17 +11,26 @@ enum Incentive {
   supply
 }
 
+enum StrategyProtocol {
+  Bedrock = 'Bedrock',
+  Solv = 'Solv',
+  Pell = 'Pell',
+  Segment = 'Segment'
+}
+
 type StrategyInfo = {
   name: string;
-  protocol: string;
-  incentives: Incentive[];
-  tvl: string;
-  about: ReactNode;
+  description: ReactNode;
+  protocol: StrategyProtocol;
+  incentives: StrategyIncentive[];
   outputToken: string;
-  securityReview: string;
-  website: string;
   isDisabled?: boolean;
   warningMessage?: ReactNode;
+  links: {
+    securityReview?: string;
+    landingPage: string;
+    manage: string;
+  };
 };
 
 const babylonWithdrawWarning = <Trans>Babylon does not yet support withdrawals.</Trans>;
@@ -29,105 +38,130 @@ const babylonWithdrawWarning = <Trans>Babylon does not yet support withdrawals.<
 const strategiesInfo: Record<string, StrategyInfo> = {
   'bedrock-unibtc': {
     name: 'Liquid Staking Bedrock-Babylon',
-    protocol: 'Bedrock',
-    incentives: [Incentive.bedrock, Incentive.babylon],
-    tvl: '-',
-    about: <Trans>Stake BTC into Babylon via Bedrock and receive liquid staking token uniBTC.</Trans>,
+    description: <Trans>Stake BTC into Babylon via Bedrock and receive liquid staking token uniBTC.</Trans>,
+    protocol: StrategyProtocol.Bedrock,
+    incentives: [StrategyIncentive.bedrock, StrategyIncentive.babylon],
     outputToken: 'uniBTC',
-    securityReview: 'https://www.bitcoinlayers.org/infrastructure/bedrock-unibtc',
-    website: 'https://app.bedrock.technology/unibtc',
+    links: {
+      securityReview: 'https://www.bitcoinlayers.org/infrastructure/bedrock-unibtc',
+      landingPage: 'https://app.bedrock.technology',
+      manage: 'https://app.bedrock.technology/unibtc'
+    },
     warningMessage: babylonWithdrawWarning
   },
   'solv-solvbtcbbn': {
     name: 'Liquid Staking Solv-Babylon',
-    protocol: 'Solv',
-    incentives: [Incentive.solv, Incentive.babylon],
-    tvl: '-',
-    about: <Trans>Stake BTC into Babylon via Solv Protocol and receive liquid staking token solvBTC.BBN.</Trans>,
+    description: <Trans>Stake BTC into Babylon via Solv Protocol and receive liquid staking token solvBTC.BBN.</Trans>,
+    protocol: StrategyProtocol.Solv,
+    incentives: [StrategyIncentive.solv, StrategyIncentive.babylon],
     outputToken: 'SolvBTC.BBN',
-    securityReview: 'https://www.bitcoinlayers.org/infrastructure/solv-solvbtcbbn',
-    website: 'https://app.solv.finance/babylon?network=bob',
+    links: {
+      securityReview: 'https://www.bitcoinlayers.org/infrastructure/solv-solvbtcbbn',
+      landingPage: 'https://app.solv.finance/babylon?network=bob',
+      manage: 'https://solv.finance/'
+    },
     warningMessage: babylonWithdrawWarning
   },
   'pell-solvbtcbbn': {
     name: 'Restaking Pell-SolvBTC-Babylon',
-    protocol: 'Pell',
-    incentives: [Incentive.pell, Incentive.solv, Incentive.babylon],
-    tvl: '-',
-    about: (
+    description: (
       <Trans>
         Stake BTC into Babylon via Solv Protocol, get solvBTC.BBN liquid staking token, and deposit into Pell.
       </Trans>
     ),
+    protocol: StrategyProtocol.Pell,
+    incentives: [StrategyIncentive.pell, StrategyIncentive.solv, StrategyIncentive.babylon],
     outputToken: 'SolvBTC.BBN',
-    securityReview: 'https://www.bitcoinlayers.org/infrastructure/solv-solvbtcbbn',
-    website: 'https://app.pell.network/restake',
+    links: {
+      securityReview: 'https://www.bitcoinlayers.org/infrastructure/solv-solvbtcbbn',
+      landingPage: 'https://app.pell.network/',
+      manage: 'https://app.pell.network/restake/detail?chainid=60808&address=0x6f0AfADE16BFD2E7f5515634f2D0E3cd03C845Ef'
+    },
     warningMessage: babylonWithdrawWarning
   },
   'pell-unibtc': {
     name: 'Restaking Pell-uniBTC-Babylon',
-    protocol: 'Pell',
-    incentives: [Incentive.pell, Incentive.bedrock, Incentive.babylon],
-    tvl: '-',
-    about: (
+    description: (
       <Trans>
         Stake BTC into Babylon via Bedrock, get uniBTC liquid staking token, and deposit into Pell restaking.
       </Trans>
     ),
+    protocol: StrategyProtocol.Pell,
+    incentives: [StrategyIncentive.pell, StrategyIncentive.bedrock, StrategyIncentive.babylon],
     outputToken: 'uniBTC',
-    securityReview: 'https://www.bitcoinlayers.org/infrastructure/bedrock-unibtc',
-    website: 'https://app.pell.network/restake',
+    links: {
+      securityReview: 'https://www.bitcoinlayers.org/infrastructure/bedrock-unibtc',
+      landingPage: 'https://app.pell.network/',
+      manage: 'https://app.pell.network/restake/detail?chainid=60808&address=0x631ae97e24f9F30150d31d958d37915975F12ed8'
+    },
     warningMessage: babylonWithdrawWarning
   },
   'segment-tbtc': {
     name: 'Lending Segment-tBTC',
-    protocol: 'Segment',
-    incentives: [Incentive.segment, Incentive.supply],
-    tvl: '-',
-    about: <Trans>Lend out tBTC on Segment.</Trans>,
+    description: <Trans>Lend out tBTC on Segment.</Trans>,
+    protocol: StrategyProtocol.Segment,
+    incentives: [StrategyIncentive.segment, StrategyIncentive.supply],
     outputToken: 'tBTC',
-    securityReview: 'https://www.bitcoinlayers.org/infrastructure/threshold-tbtc',
-    website: 'https://app.segment.finance'
+    links: {
+      securityReview: 'https://www.bitcoinlayers.org/infrastructure/threshold-tbtc',
+      landingPage: 'https://app.segment.finance',
+      manage: 'https://app.segment.finance'
+    }
   },
   'segment-wbtc': {
     name: 'Lending Segment-wBTC',
-    protocol: 'Segment',
-    incentives: [Incentive.segment, Incentive.supply],
-    tvl: '-',
-    about: <Trans>Lend out wBTC on Segment. </Trans>,
+    description: <Trans>Lend out wBTC on Segment. </Trans>,
+    protocol: StrategyProtocol.Segment,
+    incentives: [StrategyIncentive.segment, StrategyIncentive.supply],
     outputToken: 'wBTC',
-    securityReview: 'https://www.bitcoinlayers.org/infrastructure/bitgo-wbtc',
-    website: 'https://app.segment.finance'
+    links: {
+      securityReview: 'https://www.bitcoinlayers.org/infrastructure/bitgo-wbtc',
+      landingPage: 'https://app.segment.finance',
+      manage: 'https://app.segment.finance/#//market/0x6265C05158f672016B771D6Fb7422823ed2CbcDd'
+    }
   },
   'segment-sesolvbtcbbn': {
     name: 'Staked Lending Segment-SolvBTC-Babylon',
-    protocol: 'Segment',
-    incentives: [Incentive.segment, Incentive.solv, Incentive.babylon, Incentive.supply],
-    tvl: '-',
-    about: (
+    description: (
       <Trans>
         Stake BTC into Babylon via Solv Protocol, get solvBTC.BBN liquid staking token, and lend it out on Segment.
       </Trans>
     ),
-    outputToken: 'seUNIBTC',
-    securityReview: '',
-    website: 'https://app.segment.finance',
+    protocol: StrategyProtocol.Segment,
+    incentives: [
+      StrategyIncentive.segment,
+      StrategyIncentive.solv,
+      StrategyIncentive.babylon,
+      StrategyIncentive.supply
+    ],
+    outputToken: 'seSOLVBTCBBN',
+    links: {
+      landingPage: 'https://app.segment.finance',
+      manage: 'https://app.segment.finance/#//market/0x5EF2B8fbCc8aea2A9Dbe2729F0acf33E073Fa43e'
+    },
     warningMessage: babylonWithdrawWarning
   },
   'segment-seunibtc': {
     name: 'Staked Lending Segment-uniBTC-Babylon',
-    protocol: 'Segment',
-    incentives: [Incentive.segment, Incentive.bedrock, Incentive.babylon, Incentive.supply],
-    tvl: '-',
-    about: (
+    description: (
       <Trans>Stake BTC into Babylon via Bedrock, get uniBTC liquid staking token, and lend it out on Segment.</Trans>
     ),
+    protocol: StrategyProtocol.Segment,
+    incentives: [
+      StrategyIncentive.segment,
+      StrategyIncentive.bedrock,
+      StrategyIncentive.babylon,
+      StrategyIncentive.supply
+    ],
     outputToken: 'seUNIBTC',
-    securityReview: '',
-    website: 'https://app.segment.finance',
+    links: {
+      securityReview: '',
+      landingPage: 'https://app.segment.finance',
+      manage: 'https://app.segment.finance/#//market/0x7848F0775EebaBbF55cB74490ce6D3673E68773A'
+    },
     warningMessage: babylonWithdrawWarning
   }
 } as const;
 
-export { Incentive, strategiesInfo };
+export { StrategyIncentive, strategiesInfo };
 export type { StrategyInfo };
